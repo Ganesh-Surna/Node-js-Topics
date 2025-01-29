d2bfe6fd5e916ea5d97072f60628a94689292681
Ganesh Surna <140999072+Ganesh-Surna@users.noreply.github.com>
2025-01-29 16:37:15 +0530

    Update 04_ Express.js

--- 04_ Express.js
+++ 04_ Express.js
@@ -7,5 +7,20 @@ Request ->
                 next() ->
                     res.send() ->
                           Response
-  
+
+
+    const express = require('express');
+    const app = express();
+    app.use((req, res, next) => {
+        console.log('Middleware-1')
+        next(); // Allows the request to continue to the next middleware in line
+    })
+    app.use((req, res, next) => {
+        console.log('Middleware-2 executes only if next() is called in Middleware-1')
+        res.send()
+    })
+    app.listen(3000, () => console.log('Server running on port 3000'));
+
+
+res.send() -> Automatically detects the content type and sets the Content-Type in response Headers.
       
